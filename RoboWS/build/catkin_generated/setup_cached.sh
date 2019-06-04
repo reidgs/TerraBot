d9e5cc3d97f5436119dc1b0876434ebd94cb18e8
Jack <jmcamero@andrew.cmu.eedu>
2019-06-03 20:47:13 -0400

    Created a workspace enviroment in RobWS

---
+++ RoboWS/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jcmate/Desktop/robotanist/RoboWS/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/jcmate/Desktop/robotanist/RoboWS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jcmate/Desktop/robotanist/RoboWS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/jcmate/Desktop/robotanist/RoboWS/src:$ROS_PACKAGE_PATH"
