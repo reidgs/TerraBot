ca656509757f2b0f85b09cfe59b74d918713f57e
Jack <jmcamero@andrew.cmu.edu>
2019-07-02 16:28:04 -0400

    added wait to stop to make sure prints were in correct place

--- ThreeNode/stop.sh
+++ ThreeNode/stop.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 kill $(cat processes.txt)
-
+sleep 3
 DATE=$(date)
 GREEN='\033[1;32m'
 NC='\033[0m'
-echo -e "${GREEN} Closed on $DATE ${NC}" >> Log/rosserial.log
-echo -e "${GREEN} Closed on $DATE ${NC}" >> Log/student.log
-echo -e "${GREEN} Closed on $DATE ${NC}" >> Log/relay.log
-
-echo  >> Log/rosserial.log
-echo  >> Log/student.log
-echo  >> Log/relay.log
+echo -e "${GREEN} Closed on $DATE ${NC}\n" >> Log/rosserial.log
+echo -e "${GREEN} Closed on $DATE ${NC}\n" >> Log/student.log
+echo -e "${GREEN} Closed on $DATE ${NC}\n" >> Log/relay.log
