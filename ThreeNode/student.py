3e5b1d686f6b6f2d59b61fd3b6ccf2a4fc0004dc
Mike Xu <mikepjxu@gmail.com>
2019-07-11 19:55:32 -0400

    added a 12 hour light cycle and pictures every hour

--- ThreeNode/student.py
+++ ThreeNode/student.py
@@ -4,11 +4,19 @@
 import rospy
 from std_msgs.msg import Int32
 from std_msgs.msg import Bool
+from std_msgs.msg import String
+import time
 
 light = 0
-led_level = 0
+led_level = 255
 hum  = 0
 w_level = 3
+
+def get_time():
+    return time.time()
+
+light_time = get_time()
+cam_time = get_time()
 
 rospy.init_node("student", anonymous = True)
 
@@ -17,6 +25,7 @@ npump_pub = rospy.Publisher("npump_input", Bool, queue_size = 100)
 apump_pub = rospy.Publisher("apump_input", Bool, queue_size = 100)
 led_pub = rospy.Publisher("led_input", Int32, queue_size = 100)
 fan_pub = rospy.Publisher("fan_input", Bool, queue_size = 100)
+cam_pub = rospy.Publisher("cam_input", Bool, queue_size = 100)
 
 def humid_reaction(data):
     global hum
@@ -36,17 +45,28 @@ def level_reaction(data):
 def tds_reaction(data):
     True
 
-humid_sensor = rospy.Subscriber("humid_output", Int32, humid_reaction)
+def cam_reaction(data):
+    print ("picture taken\t" + data)
+
 temp_sensor = rospy.Subscriber("temp_output", Int32, temp_reaction)
 light_sensor = rospy.Subscriber("light_output", Int32, light_reaction)
 level_sensor = rospy.Subscriber("level_output", Int32, level_reaction)
 tds_sensor = rospy.Subscriber("tds_output", Int32, tds_reaction)
+cam_sensor = rospy.Subscriber("cam_raw", String, cam_reaction)
 
 while not rospy.core.is_shutdown():
-    if light < 9000:
+    time_now = get_time()
+    if time_now - light_time > 4320:
+        led_level ^= 255
+        light_time = time_now
+    """if light < 9000:
         led_level = min(255, led_level + 1)
     else:
-        led_level = max(0, led_level - 1)
+        led_level = max(0, led_level - 1)"""
+    if time_now - cam_time > 3600:
+        cam_pub.publish(True)
+        cam_time = time_now
+
     led_pub.publish(led_level)
 #    print([light,led_level])
     fan_pub.publish(True if hum > 70 else False)
