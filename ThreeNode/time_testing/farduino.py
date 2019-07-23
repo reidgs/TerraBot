dd9bbcf848b5ada0c536b664c553aa8338f1e460
Mike Xu <xpj@cmu.edu>
2019-07-23 17:16:06 -0400

    fake arduino with buggy simple callback

--- ThreeNode/time_testing/farduino.py
+++ ThreeNode/time_testing/farduino.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Int32,Bool,Float32,String
-from topic_def import sensor_names, actuator_names, to_ard, from_ard
+from topic_def import *
 
 values = {}
+publishers = {}
+subscribers = {}
 
 def generate_values():
     global values
@@ -20,44 +22,54 @@ def generate_publishers():
 
 #used when generating subs
 def update_request(name, data):
+    global values
+    print (name)
     values[name] = data.data
 
 def generate_subscribers():
     global subscribers
     for name in actuator_names:
         sub_name = name + "_raw"
-        cb = lambda data: update_request(name, data)
-        subscribers[name] = rospy.Subscriber(sub_name, to_ard[name], cb)
+        print(name)
+        subscribers[name] = rospy.Subscriber(sub_name, to_ard[name],
+            lambda data: update_request(name, data))
 
 def light_update(cur_interval):
     values['light'] = values['led']
 
 def level_update(cur_interval):
-    values['level'] = values['wpump']
+    values['level'] = 1 if values['wpump'] else 0
 
 def tds_update(cur_interval):
-    values['tds'] = values['npump']
+    values['tds'] = 1 if values['npump'] else 0
 
-def hum_update(cur_interval):
-    values['humid'] = values['level'] + values['fan']
+def humid_update(cur_interval):
+    values['humid'] = 1 if values['fan'] else 0
 
 def temp_update(cur_interval):
-    values['temp'] = values['fan']
+    values['temp'] = 1 if values['fan'] else 0
 
 def cur_update(cur_interval):
-    values['cur'] = 'idk'
+    values['cur'] = 1 if values['led'] else 0
 
-rospy.init_node('Simulator', anonymous=True)
 rospy.set_param("use_sim_time", True)
+rospy.init_node('Simulator', anonymous=True)
 cur_time = 0
 
+generate_values()
+generate_publishers()
+generate_subscribers()
+values['freq'] = 1
+
+for s in subscribers:
+    subscribers[s].callback(Int32(12))
 while  not rospy.core.is_shutdown():
-    
-    if rospy.get_time() - cur_time >= 1.0/values[freq]:
+
+    if rospy.get_time() - cur_time >= 1.0/values['freq']:
 
         cur_time = rospy.get_time()
-        cur_interval = 1.0/values[freq]
-        
+        cur_interval = 1.0/values['freq']
+        print([(k,values[k]) for k in values.keys()])
         #update sensors (calculations) + publish
         for sensor in sensor_names:
             values[sensor] = globals()[sensor + '_update'](cur_interval)
