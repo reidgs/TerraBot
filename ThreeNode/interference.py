6fda73575227cdbd0abea0d524ab580d12531bda
Cara <carali2000@gmail.com>
2019-08-14 16:39:00 -0400

    array msgs for redundant sensors

--- ThreeNode/interference.py
+++ ThreeNode/interference.py
@@ -1,8 +1,8 @@
 import rospy
-from std_msgs.msg import Int32,Bool,Float32,String
+from std_msgs.msg import Int32,Bool,Float32,String,Int32MultiArray,Float32MultiArray
 from topic_def import *
 
-act_interf = {}
+interf_funcs = {}
 schedules = {}
 next_update ={}
 
@@ -20,7 +20,7 @@ def noise(x):
 
 ### default ###
 for n in sensor_names + actuator_names:
-    act_interf[n] = identity
+    interf_funcs[n] = identity
     schedules[n] = {}
     next_update[n] = -1
 
@@ -32,10 +32,14 @@ def parse_interf(path=None):
         lines = [l.strip().split(",") for l in f.readlines()]
     lst = lines
     for l in lst:
-        schedules[l[1]][l[0]] = l[2]
+        #redundant sensors
+        if l[1] != 'level' and l[1] in sensor_names:
+            schedules[l[1]][l[0]] = [l[2],l[3]]
+        else:
+            schedules[l[1]][l[0]] = l[2]
     for s in schedules:
         if len(schedules[s]) > 0:
-            next_update[l[1]] = min(schedules[l[1]], key=int)
+            next_update[s] = min(schedules[s], key=int)
 
 states_funcs = {
     'normal' : identity,
@@ -49,11 +53,22 @@ states_funcs = {
 def get_inter(name, time):
     if next_update[name] != -1 and \
             time.to_sec() >= int(next_update[name]):
+
         state = schedules[name].pop(next_update[name])
         next_update[name] = min(schedules[name], key=int) \
                 if len(schedules[name]) > 0 else -1
-        act_interf[name] = states_funcs[state]
-    return act_interf[name]
+        
+        #redundant sensors
+        if type(state) == list:
+            temp = []
+            for x in state:
+                temp.append(states_funcs[x])
+            interf_funcs[name] = temp
+
+        else:
+            interf_funcs[name] = states_funcs[state]
+
+    return interf_funcs[name]
 
 
 
