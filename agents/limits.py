abdb288d936738b42ec5add37019d31ce1ab5eb7
Jack <jmcamero@andrew.cmu.edu>
2020-08-27 23:07:17 -0400

    Added an "optimal" dictionary, for the 'best' value to achieve for various sensors

--- agents/limits.py
+++ agents/limits.py
@@ -1,9 +1,11 @@
 scale = {}
 limits = {}
+optimal = {} # Note, not all the sensors have 'optimal' levels
 names = {}
 
 scale['light_level'] = [0, 1000]
 limits['light_level'] = [800, 950]
+optimal['light_level'] = 900
 names['light_level'] = 'lights'
 
 scale['water_level'] = [0, 200]
@@ -12,14 +14,17 @@ names['water_level'] = 'w_level'
 
 scale['moisture'] = [0, 1000]
 limits['moisture'] = [450, 650]
+optimal['moisture'] = 600
 names['moisture'] = 'moist'
 
 scale['humidity'] = [0, 100]
 limits['humidity'] = [75, 90]
+optimal['humidity'] = 80
 names['humidity'] = 'humid'
 
 scale['temperature'] = [10, 40] # Celcius
 limits['temperature'] = [24, 30]
+optimal['temperature'] = 26
 names['temperature'] = 'temp'
 
 scale['current'] = [0, 1000]
