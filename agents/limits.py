d64321d387c598e925225493bf5aed3edf618795
Stephanie Rosenthal <srosenth@andrew.cmu.edu>
2020-08-28 14:00:13 -0400

    adding optimal values

--- agents/limits.py
+++ agents/limits.py
@@ -1,25 +1,31 @@
 scale = {}
 limits = {}
+optimal = {}
 names = {}
 
 scale['light_level'] = [0, 1000]
 limits['light_level'] = [800, 950]
+optimal['light_level'] = [850,950]
 names['light_level'] = 'lights'
 
 scale['water_level'] = [0, 200]
 limits['water_level'] = [5, 200]
+optimal['water_level'] = [5,200]
 names['water_level'] = 'w_level'
 
 scale['moisture'] = [0, 1000]
 limits['moisture'] = [450, 650]
+optimal['moisture'] = [500,650]
 names['moisture'] = 'moist'
 
 scale['humidity'] = [0, 100]
 limits['humidity'] = [75, 90]
+optimal['humidity'] = [60, 80]
 names['humidity'] = 'humid'
 
 scale['temperature'] = [10, 40] # Celcius
 limits['temperature'] = [24, 30]
+optimal['temperature'] = [24,29]
 names['temperature'] = 'temp'
 
 scale['current'] = [0, 1000]
