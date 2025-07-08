cad6e7d6dedb7eb05478c21d5a906d5a69e8e196
reidgs <rsimmons@andrew.cmu.edu>
2025-07-08 16:40:53 -0400

    Put the message handler in a separate thread - wasn't working well o/w

--- lib/farduino.py
+++ lib/farduino.py
@@ -5,22 +5,19 @@
 
 ### Import used files
 import rclpy, rclpy.node
-from std_msgs.msg import Int32,Bool,Float32,String,Int32MultiArray,Float32MultiArray
-from topic_def import *
+from std_msgs.msg import Int32
 from rosgraph_msgs.msg import Clock
-from rclpy.time import Time
-import time
-import argparse                                                    
-from baseline import Baseline
-from os.path import abspath
+import time, sys, argparse, os.path, signal
+from functools import partial
+from threading import Thread, Lock
+from datetime import datetime
 
+from topic_def import *
+from freqmsg import frommsg
+from terrabot_utils import clock_time, get_ros_time, set_use_sim_time
 import environment as env
 from render import Terrarium
-from freqmsg import frommsg
-from direct.stdpy import threading
-import sys
-from datetime import datetime
-from terrabot_utils import clock_time, get_ros_time, set_use_sim_time
+from baseline import Baseline
 
 class Simulator(rclpy.node.Node):
     def __init__(self, speedup):
@@ -28,37 +25,42 @@ class Simulator(rclpy.node.Node):
         self.default_speedup = self.speedup = speedup
         set_use_sim_time(self, True)
         self.clock_pub = None
+        self.lock = Lock()
+        self.received = {}
         self.generate_publishers()
         self.generate_subscribers()
 
     ### Handle Sub/Pub
 
     ## Actuator Callbacks
-
-    def freq_cb(self, data):
-        # Parse and update sensor frequency
-        name, freq = frommsg(data.data)
-        sensor_timing[name][1] = (99999999 if freq == 0 else 1/freq)
-    
-    def speedup_cb(self, data):
-        self.default_speedup = self.speedup = data.data
-
-    def camera_cb(self, data):
-        global renderer
-        renderer.takeAndStorePic(data.data)
+    def action_cb(self, action, data):
+        # To avoid collisions, just squirrel the data away;
+        # Data is processed by update_actions
+        with self.lock: self.received[action] = data
 
     def generate_subscribers(self):
-        actuator_cbs = \
-          { 'led' : (lambda data : env.params.update({'led' : data.data})),
-            'wpump' : (lambda data : env.params.update({'wpump' : data.data})),
-            'fan' : (lambda data : env.params.update({'fan' : data.data})),
-            'freq' : self.freq_cb,
-            'camera': self.camera_cb,}
-
         for name in actuator_names: 
             self.create_subscription(actuator_types[name], name+'_raw',
-                                     actuator_cbs[name], 1)
-        self.create_subscription(Int32, 'speedup', self.speedup_cb, 1)
+                                     partial(self.action_cb, name), 1)
+        self.create_subscription(Int32, 'speedup', 
+                                 partial(self.action_cb, 'speedup'), 1)
+        
+    def update_actions(self):
+        global renderer
+        with self.lock:
+            for action in self.received:
+                data = self.received[action]
+                if action == 'freq':
+                    # Parse and update sensor frequency
+                    name, freq = frommsg(data.data)
+                    sensor_timing[name][1] = (99999999 if freq == 0 else 1/freq)
+                elif action == 'speedup':
+                    self.default_speedup = self.speedup = data.data
+                elif action == 'camera':
+                    renderer.takeAndStorePic(data.data)
+                else:
+                    env.params.update({action : data.data})
+            self.received = {}
 
     ## Sensor Setup
 
@@ -82,8 +84,9 @@ class Simulator(rclpy.node.Node):
                       'weight' : [0.0, 1.0]}
     
     def publish (self, name, value):
-        msg = sensor_types[name](data=value)
-        self.pubs[name].publish(msg)
+        with self.lock:
+            msg = sensor_types[name](data=value)
+            self.pubs[name].publish(msg)
 
     # These functions sense from the environment and publish to their topic
 
@@ -116,47 +119,17 @@ class Simulator(rclpy.node.Node):
                 self.sensor_timing[name][0] = 0 #Reset cooldown
                 eval("self.sense_%s()" % name) #Call the sensor function
 
-### Parse arguments
-
-parser = argparse.ArgumentParser(description='simulation parser for Autonomous Systems')
-parser.add_argument('--baseline', type = str, default = None, nargs = "?")
-parser.add_argument('--speedup', type = float, default = 1, nargs = "?")
-parser.add_argument('--graphics', default = False, action = 'store_true')
-parser.add_argument('-l', '--log', action = 'store_true')
-args = parser.parse_args()
-
-### RUNTIME ###
-
-## handle env init stuff
-
-# extract baseline values
-bl = None
-if args.baseline:
-    try:
-        bl = Baseline(abspath(args.baseline))
-    except:
-        print('baseline file %s not found or parse error' %args.baseline)
-        exit()
-
-env.init(bl)
-
-max_speedup_pump = 1
-max_speedup_fan = 100
-
-## handle ROS init stuff
-rclpy.init()
-simulator = Simulator(args.speedup)
-
-### Sim loop (Put here for threading purposes)
-doloop = True
-
-t0 = int(datetime(2000, 1, 1).strftime('%s')) # Initialize to 01-00:00:00, for display purposes
-
 def publish_clock(sim, now):
     clock = Clock()
     clock.clock.sec = int(now)
     clock.clock.nanosec = int((now - clock.clock.sec) * 1e9)
     sim.clock_pub.publish(clock)
+
+# Have to use this, rather than spin(), which seems to get blocked by panda3d
+def spin_fn(node):
+    while rclpy.ok():
+        rclpy.spin_once(node, timeout_sec=0.1)
+    print("Spin thread exiting")
 
 def sim_loop():
     global doloop, simulator, renderer
@@ -168,13 +141,12 @@ def sim_loop():
     if bl is not None:
         now += bl.params['start']
     publish_clock(simulator, now) # Publish initial time
-    while get_ros_time(simulator) == 0:
-        rclpy.spin_once(simulator, timeout_sec=0.1)
 
     while doloop:
-        
-        rclpy.spin_once(simulator, timeout_sec=0.01)
-        time.sleep(tick_time-0.01) # Wait for next tick
+
+        time.sleep(tick_time) # Wait for next tick
+
+        simulator.update_actions()
         
         # speedup should be maxed if pumping/fanning
         simulator.speedup = min(simulator.default_speedup,
@@ -189,7 +161,8 @@ def sim_loop():
             pump_last_off_time = now
         elif (env.params['wpump']):
             pump_last_on = True
-        if (now - pump_last_off_time < 10): simulator.speedup = 1
+        if (now - pump_last_off_time < 10):
+            simulator.speedup = 1
         now += tick_time * simulator.speedup
         publish_clock(simulator, now)
 
@@ -203,10 +176,9 @@ def sim_loop():
         renderer.update_env_params(env.params, simulator.speedup,
                                    env.light_average(), env.get_weight())
     #Stop panda window
-    #if renderer is not None:
-    #    renderer.userExit()
-    
-    
+    if renderer is not None:
+        renderer.userExit()
+
 #Init graphics
 def init_graphics(sim, use_graphics):
     droop = 0
@@ -225,20 +197,56 @@ def init_graphics(sim, use_graphics):
                                env.light_average(), env.get_weight())
     return renderer
 
-
-#Start sim loop THEN panda
-
-import signal
 def handler(signum, frame):
-    global thread, doloop
+    global thread, doloop, spin_thread
     doloop = False
     thread.join()
+    rclpy.shutdown()
+    spin_thread.join()
     sys.exit()
-    
+
+### Parse arguments
+parser = argparse.ArgumentParser(description='simulation parser for Autonomous Systems')
+parser.add_argument('--baseline', type = str, default = None, nargs = "?")
+parser.add_argument('--speedup', type = float, default = 1, nargs = "?")
+parser.add_argument('--graphics', default = False, action = 'store_true')
+parser.add_argument('-l', '--log', action = 'store_true')
+args = parser.parse_args()
+
+### RUNTIME ###
+
+# extract baseline values
+bl = None
+if args.baseline:
+    try:
+        bl = Baseline(os.path.abspath(args.baseline))
+    except:
+        print('baseline file %s not found or parse error' %args.baseline)
+        exit()
+
+## handle env init stuff
+env.init(bl)
+
+max_speedup_pump = 1
+max_speedup_fan = 100
+
+## handle ROS init stuff
+rclpy.init()
+simulator = Simulator(args.speedup)
+# Put the message handler in its own thread
+spin_thread = Thread(target=spin_fn, args=(simulator,), daemon=True)
+spin_thread.start()
+
+### Sim loop (Put here for threading purposes)
+doloop = True
+
+t0 = int(datetime(2000, 1, 1).strftime('%s')) # Initialize to 01-00:00:00, for display purposes
+
+#Start sim loop THEN panda
 signal.signal(signal.SIGTERM, handler)
 
 renderer = init_graphics(simulator, args.graphics)
-thread = threading.Thread(target=sim_loop)
+thread = Thread(target=sim_loop)
 thread.start()
 
 renderer.run()
